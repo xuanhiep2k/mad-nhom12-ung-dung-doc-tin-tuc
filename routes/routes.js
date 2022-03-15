51c6daa989a0af20ac85a067fce3fe5624be992a
xuanhiep2k <tranxuanhiepcmd3@gmail.com>
2022-03-15 23:32:00 +0700

    add login route

--- routes/routes.js
+++ routes/routes.js
@@ -25,5 +25,33 @@ routes.post('/register', async(req, res) => {
     }
 })
 
+//route login
+routes.post('/login', async(req, res) => {
+    try {
+        const { email, password} = req.body;
+        if(!email || !password){
+            res.status(400).json({
+                message: `Missing ${!email ? 'email' : 'password'}!`
+            })
+        }
+        
+        await connectDB.query(`SELECT * FROM users WHERE email = '${email}'`, (error, data) => {
+            if(error) {
+                console.log("Not found email")
+            }
+            const user = JSON.parse(JSON.stringify(data))[0];
+            const isValidate = bcrypt.compareSync(password, user.password);
+            if(isValidate){
+                res.status(200).json({message: "Login successfully!"});
+            }else{
+                res.status(400).json({message: "Login fail!"});
+            }
+            
+        })
+    } catch (error) {
+        res.status(400).json({error});
+        process.exit(1);
+    }
+})
 
 module.exports = routes;
